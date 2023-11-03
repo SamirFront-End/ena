f65a49fd3c6b219483be49a1dd605d68639076d9
SamirFront-End <miramirez1987@gmail.com>
2023-11-03 17:20:21 -0500

    terminando index desktop

--- js/carousel/carousel.js
+++ js/carousel/carousel.js
@@ -1,6 +1,8 @@
 const carouselCont = document.querySelector('.carousel-container');
 const ctrl = document.querySelectorAll('.ctrl');
 let currentIndex = 0;
+let isMobile = window.innerWidth < 768;
+let interval;
 
 function moveToIndex(index) {
     const mov = index * -33.3;
@@ -20,13 +22,29 @@ ctrl.forEach((control, i) => {
     });
 });
 function autoSlide() {
-    const nextIndex = (currentIndex + 1) % ctrl.length;
-    moveToIndex(nextIndex);
+    if (isMobile) {
+        const nextIndex = (currentIndex + 1) % ctrl.length;
+        moveToIndex(nextIndex);
+    }
+}
+if (isMobile) {
+    const interval = setInterval(autoSlide, 5000);
 }
 
-const interval = setInterval(autoSlide, 5000);
 // ctrl.forEach((control, i) => {
 //     ctrl[i].addEventListener('click', () => {
 //         clearInterval(interval);
 //     });
 // });
+window.addEventListener('resize', () => {
+    const newIsMobile = window.innerWidth < 768;
+
+    if (!isMobile && newIsMobile) {
+        isMobile = newIsMobile;
+        interval = setInterval(autoSlide, 5000);
+    } else if (isMobile && !newIsMobile) {
+        isMobile = newIsMobile;
+        clearInterval(interval);
+        moveToIndex(0);
+    }
+});
